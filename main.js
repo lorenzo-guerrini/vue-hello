e5dfaac56268336ebbbebaecc61173dccf314200
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-01-16 18:23:29 +0100

    Bonus finito

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let vue = new Vue({
     el: "#myApp",
     data: {
-        message: "Hello World!"
+        message: "Hello World!",
+        image: "https://image.shutterstock.com/image-photo/smiling-diverse-colleagues-gather-boardroom-600w-1687550977.jpg"
     }
 })
